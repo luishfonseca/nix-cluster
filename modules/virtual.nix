020ea318675086f570c0ae39a711ab9a8dd290e9
Luís Fonseca <luis@lhf.pt>
2022-12-11 22:46:23 +0000

    Minor formatting

--- modules/virtual.nix
+++ modules/virtual.nix
@@ -5,8 +5,7 @@ let cfg = config.cluster.virtual; in
 {
   options.cluster.virtual = mkEnableOption "Virtual Machine";
 
-  config = mkIf cfg ({
-    services.getty.autologinUser = "root";
-  } // import (modulesPath + "/profiles/minimal.nix") { inherit config lib; }
-  // import (modulesPath + "/profiles/qemu-guest.nix") { inherit config lib; });
+  config = mkIf cfg ({ services.getty.autologinUser = "root"; }
+    // import (modulesPath + "/profiles/minimal.nix") { inherit config lib; }
+    // import (modulesPath + "/profiles/qemu-guest.nix") { inherit config lib; });
 }
